da8c4e68e80f31cb35046893e23e1c8c918f33c3
SingleAccretion <AccretionMail@yandex.ru>
2020-08-10 17:07:47 +0300

    Add a demo image for the expression logger.

--- Accretion.Diagnostics.ExpressionLogger.ManualTesting/Program.cs
+++ Accretion.Diagnostics.ExpressionLogger.ManualTesting/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace Accretion.Diagnostics.ExpressionLogger.ManualTesting
 {
@@ -8,8 +7,8 @@ namespace Accretion.Diagnostics.ExpressionLogger.ManualTesting
         public static void Main()
         {
             ExpressionLogger.Log(new Action(() => Console.WriteLine(10)));
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(IsEmpty(new int[] { }));
+
+            IsEmpty(new int[] { });
 
             1.Log();
 
@@ -19,20 +18,13 @@ namespace Accretion.Diagnostics.ExpressionLogger.ManualTesting
             kek.Log();
 
             ConsoleColor.Black.ToString().Log();
+
             ConsoleColor.Black.ToString()?.Log();
-
-            Console.WriteLine(new Action(() => Console.WriteLine(10)));
-
-            foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
-            {
-                Console.ForegroundColor = color;
-                Console.WriteLine($"This is {Enum.GetName(typeof(ConsoleColor), color)}");
-            }
         }
 
         public static bool IsEmpty(Array array)
         {
-            ExpressionLogger.Log(array.Length);
+            array.Length.Log();
             return array.Length > 0;
         }
     }
